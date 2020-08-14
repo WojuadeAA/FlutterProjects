f0f479b4b77fdf66f71ccca2a3ef847faae79b49
Wojuade Abdul Afeez <wojucrown7@gmail.com>
2020-08-14 10:50:45 +0100

    10:50

---
+++ expenseApp/lib/widgets/Transaction_Item.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../models/transaction.dart';
+
+class TransactionItem extends StatelessWidget {
+  const TransactionItem({
+    Key key,
+    @required this.transaction,
+    @required this.deleteTransaction,
+  }) : super(key: key);
+
+  final Transaction transaction;
+  final Function deleteTransaction;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4,
+      margin: EdgeInsets.symmetric(
+        vertical: 8,
+        horizontal: 5,
+      ),
+      child: ListTile(
+        leading: CircleAvatar(
+          radius: 30,
+          child: Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: FittedBox(
+              child: Text(
+                'N '
+                '${transaction.amount.toStringAsFixed(2)}',
+              ),
+            ),
+          ),
+        ),
+        title: Text(
+          transaction.title,
+          style: Theme.of(context).textTheme.title,
+        ),
+        subtitle: Text(
+          DateFormat.yMMMMd().format(transaction.date),
+        ),
+        trailing: MediaQuery.of(context).size.width > 450
+            ? FlatButton.icon(
+                icon: Icon(Icons.delete),
+                label: Text('delete'),
+                textColor: Theme.of(context).errorColor,
+                onPressed: () =>
+                    deleteTransaction(transaction.id),
+              )
+            : IconButton(
+                icon: Icon(Icons.delete),
+                color: Theme.of(context).errorColor,
+                onPressed: () =>
+                    deleteTransaction(transaction.id),
+              ),
+      ),
+    );
+  }
+}
