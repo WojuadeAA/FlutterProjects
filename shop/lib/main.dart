f5f3edcd6d813cbf309acdfb40d79689cf118b92
Wojuade Abdul Afeez <wojucrown7@gmail.com>
2020-08-25 15:03:44 +0100

    vscode

--- shop/lib/main.dart
+++ shop/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 
 import './Screens/product_detail_screen.dart';
 import './Screens/products_overview_screen.dart';
+import './providers/products_provider.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,23 +12,27 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Shop',
-      theme: ThemeData(
-        primarySwatch: Colors.teal,
-        accentColor: Colors.pinkAccent,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-        fontFamily: 'Lato',
-        textTheme: GoogleFonts.aBeeZeeTextTheme(
-          Theme.of(context).textTheme,
+    return ChangeNotifierProvider(
+      builder: (ctx) => Products(),
+      child: MaterialApp(
+        title: 'Shop',
+        theme: ThemeData(
+          primarySwatch: Colors.teal,
+          accentColor: Colors.pinkAccent,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          fontFamily: 'Lato',
+          textTheme: GoogleFonts.aBeeZeeTextTheme(
+            Theme.of(context).textTheme,
+          ),
         ),
+        routes: {
+          ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+        },
+        home: ProductsOverviewScreen(),
       ),
-      routes: {
-        ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-      },
-      home: ProductsOverviewScreen(),
     );
   }
 }
