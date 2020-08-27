40906659050948cc40e6958141c110357f067d3e
Wojuade Abdul Afeez <wojucrown7@gmail.com>
2020-08-27 10:30:41 +0100

    august 27 thursday 1029 HOURS

---
+++ shop/lib/widgets/app_drawer.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:shop/Screens/order_screen.dart';
+
+class AppDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Column(
+        children: <Widget>[
+          AppBar(
+            title: Text('hello friend'),
+            automaticallyImplyLeading: false,
+          ),
+          Divider(),
+          ListTile(
+              leading: Icon(Icons.shop),
+              title: Text('Shop Now'),
+              onTap: () {
+                Navigator.of(context).pushReplacementNamed('/');
+              }),
+          Divider(),
+          ListTile(
+              leading: Icon(Icons.payment),
+              title: Text('Orders'),
+              onTap: () {
+                Navigator.of(context)
+                    .pushReplacementNamed(OrdersScreen.routeName);
+              })
+        ],
+      ),
+    );
+  }
+}
