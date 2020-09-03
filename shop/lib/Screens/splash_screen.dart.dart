7a240fe8e37fe7e882539f05e1ea0d2ef16258c2
Wojuade Abdul Afeez <wojucrown7@gmail.com>
2020-09-03 09:35:08 +0100

    splashScreen

--- shop/lib/Screens/splash_screen.dart.dart
+++ shop/lib/Screens/splash_screen.dart.dart
@@ -4,8 +4,19 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Loading...'),
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(0, 255, 255, 1).withOpacity(0.5),
+              Color.fromRGBO(255, 20, 147, 1).withOpacity(0.9),
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            stops: [0, 1],
+          ),
+        ),
+        child: SingleChildScrollView(),
       ),
     );
   }
